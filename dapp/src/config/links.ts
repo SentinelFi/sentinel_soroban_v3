0a44110a88d238101f72380a0dbcf565ab504c6c
Jeff G <jeffoperates@gmail.com>
2026-08-06 18:16:59 +0300

    Dapp version (#116)

--- dapp/src/config/links.ts
+++ dapp/src/config/links.ts
@@ -1,5 +1,6 @@
 /** External links shared across the app chrome (footer, info bubbles). */
 export const GITHUB_URL = "https://github.com/SentinelFi"
+export const REPO_URL = "https://github.com/SentinelFi/sentinel_soroban_v3"
 export const X_URL = "https://x.com/sentinel_fi/"
 export const STELLAR_URL = "https://stellar.org/"
 export const DOCS_URL = "https://sentinelfi.github.io/sentinel_soroban_v3/"
